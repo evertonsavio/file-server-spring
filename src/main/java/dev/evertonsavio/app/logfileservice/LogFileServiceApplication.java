ccf134735e8f8fb76b0831420756c5d46eab8a20
Havyx <everluca@outlook.com>
2021-08-23 15:04:04 -0300

    :tada: log file service

---
+++ src/main/java/dev/evertonsavio/app/logfileservice/LogFileServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.evertonsavio.app.logfileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogFileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogFileServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/evertonsavio/app/logfileservice/LogFileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.evertonsavio.app.logfileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogFileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
